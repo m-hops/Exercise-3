e7583fe791e0ee95e8b196da5b70b9779b5503f9
rabbit-isdead <madeline.hops@gmail.com>
2021-11-21 22:17:40 -0500

    First Build Complete

--- public_html/displayGasha.php
+++ public_html/displayGasha.php
@@ -1,33 +1,4 @@
-<?php
-  require('openDB.php');
 
-  try{
-
-    $sql_def = "SELECT * FROM gashaCollection ORDER BY RANDOM() LIMIT 1";
-    //$sql_def = "SELECT * FROM gashaCollection";
-    // the result set
-    $result = $file_db->query($sql_def);
-
-    if (!$result) die("Cannot execute query.");
-    //var_dump($result);
-
-    if($row = $result->fetch(PDO::FETCH_ASSOC)) {
-      $name = $row['name'];
-      $music = $row['music'];
-      $img = $row['image'];
-
-      echo "<p id=\"pullNameDisplay\">" . $name . "</p>";
-      echo "<img id=\"pullImageDisplay\" src=\"images/" . $img. "\"/>";
-      echo "<p id=\"pullMusicDisplay\">" . $music . "</p>";
-    }
-
-    $file_db = null;
-  }
-  catch(PDOException $e) {
-     // Print PDOException message
-     echo $e->getMessage();
-   }
-?>
 
 <!DOCTYPE html>
 <head>
@@ -43,12 +14,41 @@
   <section class='instructionArea'>
   </section>
 
-  <section class='pullOutput'>
+  <section class ='capsule_output'>
+    <?php
+      require('openDB.php');
+
+      try{
+
+        $sql_def = "SELECT * FROM gashaCollection ORDER BY RANDOM() LIMIT 1";
+        //$sql_def = "SELECT * FROM gashaCollection";
+        // the result set
+        $result = $file_db->query($sql_def);
+
+        if (!$result) die("Cannot execute query.");
+        //var_dump($result);
+
+        if($row = $result->fetch(PDO::FETCH_ASSOC)) {
+          $name = $row['name'];
+          $music = $row['music'];
+          $img = $row['image'];
+
+          echo "<img id=\"pullImageDisplay\" src=\"images/" . $img. "\"/>";
+          echo "<img id=\"pullCardBKG\" src=\"media/cardBase.png\"/>";
+          echo "<img id=\"youPulledIMG\" src=\"media/youPulled.png\"/>";
+          echo "<img id=\"pullMusicGenre\" src=\"media/pullMusicGenre.png\"/>";
+          echo "<p id=\"pullNameDisplay\">" . $name . "</p>";
+          echo "<p id=\"pullMusicDisplay\">" . $music . "</p>";
+        }
+
+        // $file_db = null;
+      }
+      catch(PDOException $e) {
+         // Print PDOException message
+         echo $e->getMessage();
+       }
+    ?>
   </section>
-
-
-
-
 
 </section>
 
--- public_html/js/pullGasha.js
+++ public_html/js/pullGasha.js
@@ -1,16 +1,16 @@
 $(document).ready (function(){
 
-  let form = $('#capsule_data')[0];
+  let form = $('capsule_output');
   let data = new FormData(form);
 
   $.ajax({
+      type:"POST",
        url: "../../db/GashaPon.db",
        data: data,
        dataType: "text", /*response will be text */
        cache: false,
        timeout: 600000,
        success: function (response) {
-       let parsedJSON = JSON.parse(response);
        console.log(parsedJSON);
       },
       error:function(){
@@ -18,19 +18,3 @@ $(document).ready (function(){
    }
  });
  });
-
- // function displayPull(genres){
- //
- //   let htmlForm = $("#form_options");
- //   for(let i = 0; i != genres.length; ++i){
- //     let htmlInput = $("<input>");
- //     $(htmlInput).attr('type', 'radio');
- //     $(htmlInput).attr('name', 'capsule_mus');
- //     $(htmlInput).attr('value', genres[i]);
- //     $(htmlInput).appendTo(htmlForm);
- //     let htmlLable = $("<label>");
- //     $(htmlLable).attr('for', genres[i]);
- //     $(htmlLable).text(genres[i]);
- //     console.log(genres[i]);
- //   }
- // }
