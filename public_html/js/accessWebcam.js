f426fe3b40484221b39dfc815367433f7159cee3
rabbit-isdead <madeline.hops@gmail.com>
2021-11-23 00:51:25 -0500

    Final touch ups

--- public_html/js/accessWebcam.js
+++ public_html/js/accessWebcam.js
@@ -1,5 +1,8 @@
 //CITATION: https://github.com/jhuckaby/webcamjs//
 
+let takePhoto = new Audio('audio/click.mp3')
+let confirmPhoto = new Audio('audio/acceptPhoto.mp3')
+
 $(document).ready(function() {
 
   Webcam.attach('#my_camera');
@@ -9,6 +12,8 @@ $(document).ready(function() {
 function take_snapshot() {
   // take snapshot and get image data
   Webcam.snap(function(data_uri) {
+
+    takePhoto.play();
     // display results in page
     document.getElementById('results').innerHTML =
       '<img id="imageprev" src="'+data_uri+'"/>';
@@ -20,6 +25,8 @@ function saveSnap() {
   var base64image = document.getElementById("imageprev").src;
 
   Webcam.upload(base64image, 'upload.php', function(code, text) {
+
+    confirmPhoto.play();
     console.log('Save successfully');
     //console.log(text);
   });
